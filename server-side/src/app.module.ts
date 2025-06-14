c8cceea0c25f2e0ae1511363c9e5426533f485e6
iknowozey <iknowozey@gmail.com>
2025-06-15 00:40:51 +0700

    feat(auth): add user service, JWT config and AuthDto

--- server-side/src/app.module.ts
+++ server-side/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 @Module({
-	imports: [ConfigModule.forRoot()]
+	imports: [ConfigModule.forRoot(), AuthModule, UserModule]
 })
 export class AppModule {}
