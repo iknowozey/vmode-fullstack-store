eefc3ae38fd0c7acef86223a8b39b7cc7b5ae30a
iknowozey <iknowozey@gmail.com>
2025-06-23 01:26:43 +0700

    feat: created a toggle theme button

---
+++ client-side/src/components/ui/ToggleTheme.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { Moon, Sun } from 'lucide-react'
+import { useTheme } from 'next-themes'
+import * as React from 'react'
+import { Button } from './Button'
+
+export function ToggleTheme() {
+	const { theme, setTheme } = useTheme()
+
+	const toggleTheme = () => {
+		if (theme === 'dark') {
+			setTheme('light')
+		} else {
+			setTheme('dark')
+		}
+	}
+
+	return (
+		<Button variant='outline' size='default' onClick={toggleTheme}>
+			<Sun className='h-[2.4rem] w-[2.4rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90' />
+			<Moon className='absolute h-[2.4rem] w-[2.4rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0' />
+			<span className='sr-only'>Сменить тему</span>
+		</Button>
+	)
+}
