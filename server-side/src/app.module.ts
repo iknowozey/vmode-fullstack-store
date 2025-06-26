861205a5a2e5af00e1b1a8847b63fae82a9826c8
iknowozey <iknowozey@gmail.com>
2025-06-26 22:16:08 +0700

    feat(NestJS): the essence of the reviews is described

--- server-side/src/app.module.ts
+++ server-side/src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common'
+
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
-import { UserModule } from './user/user.module'
-import { ColorModule } from './color/color.module'
 import { CategoryModule } from './category/category.module'
+import { ColorModule } from './color/color.module'
 import { FileModule } from './file/file.module'
-import { StoreModule } from './store/store.module'
 import { OrderModule } from './order/order.module'
 import { StatisticsModule } from './statistics/statistics.module'
+import { StoreModule } from './store/store.module'
+import { UserModule } from './user/user.module'
 import { ProductModule } from './product/product.module'
+import { ReviewModule } from './review/review.module'
+
 @Module({
 	imports: [
 		ConfigModule.forRoot(),
@@ -20,7 +23,8 @@ import { ProductModule } from './product/product.module'
 		StoreModule,
 		OrderModule,
 		StatisticsModule,
-		ProductModule
+		ProductModule,
+		ReviewModule
 	]
 })
 export class AppModule {}
