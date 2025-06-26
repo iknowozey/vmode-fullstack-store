e67ebefab1f103b1eff33b8100e6d31bcae56de9
iknowozey <iknowozey@gmail.com>
2025-06-26 20:58:12 +0700

    feat: added page with orders (dashboard)

--- client-side/src/app/(root)/dashboard/Dashboard.tsx
+++ client-side/src/app/(root)/dashboard/Dashboard.tsx
@@ -1,10 +1,21 @@
 'use client'
 
+import { useMutation } from '@tanstack/react-query'
+import { LogOut } from 'lucide-react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
+import { Button } from '@/components/ui/Button'
+import { DataTable } from '@/components/ui/data-table/DataTable'
+import { useProfile } from '@/hooks/useProfile'
 import { saveTokenStorage } from '@/services/auth/auth-token.service'
+import { authService } from '@/services/auth/auth.service'
+import { EnumOrderStatus } from '@/shared/types/order.interface'
+import { IOrderColumn, orderColumns } from './OrderColumns'
+import { formatDate } from '@/lib/date/format-date'
+import { formatPrice } from '@/lib/string/format-price'
 
 export function Dashboard() {
+	const router = useRouter()
 	const searchParams = useSearchParams()
 
 	useEffect(() => {
@@ -13,5 +24,32 @@ export function Dashboard() {
 		if (accessToken) saveTokenStorage(accessToken)
 	}, [searchParams])
 
-	return <div>Dashboard</div>
+	const { user } = useProfile()
+
+	const { mutate: logout } = useMutation({
+		mutationKey: ['logout'],
+		mutationFn: () => authService.logout(),
+		onSuccess: () => router.push('/auth')
+	})
+
+	if (!user) return null
+
+	const formattedOrders: IOrderColumn[] = user.orders.map(order => ({
+		createdAt: formatDate(order.createdAt),
+		status: order.status === EnumOrderStatus.PENDING ? 'В ожидании' : 'Оплачен',
+		total: formatPrice(order.total)
+	}))
+
+	return (
+		<div className='my-6'>
+			<div className='flex items-center justify-between mb-4'>
+				<h1 className='text-2xl font-bold'>Ваши заказы</h1>
+				<Button variant='ghost' onClick={() => logout()}>
+					<LogOut />
+					Выйти
+				</Button>
+			</div>
+			<DataTable columns={orderColumns} data={formattedOrders} />
+		</div>
+	)
 }
