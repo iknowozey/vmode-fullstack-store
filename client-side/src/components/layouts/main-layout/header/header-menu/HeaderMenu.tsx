15596f0a920e94c3400030103249e486c7eb222d
iknowozey <iknowozey@gmail.com>
2025-07-01 03:13:54 +0700

    fix(ui): fixed display of products in the shopping cart

--- client-side/src/components/layouts/main-layout/header/header-menu/HeaderMenu.tsx
+++ client-side/src/components/layouts/main-layout/header/header-menu/HeaderMenu.tsx
@@ -13,58 +13,56 @@ import { useProfile } from '@/hooks/useProfile'
 import { HeaderCart } from './header-cart/HeaderCart'
 
 export function HeaderMenu() {
-  const { user, isLoading } = useProfile()
+	const { user, isLoading } = useProfile()
 
-  return (
-    <div className='hidden items-center gap-x-4 ml-auto lg:flex'>
-      <HeaderCart />
-      <Link href={PUBLIC_URL.explorer()}>
-        <Button variant='ghost'>Каталог</Button>
-      </Link>
-      {isLoading ? (
-        <>
-          <div className='flex gap-x-7 items-center'>
-            <Skeleton className='h-8 w-84' />
-            <Loader size='default' />
-          </div>
-        </>
-      ) : user ? (
-        <>
-          <Link href={DASHBOARD_URL.favorites()}>
-            <Button variant='ghost'>Избранное</Button>
-          </Link>
-          {user.stores.length ? (
-            <Link href={STORE_URL.home(user.stores[0].id)}>
-              <Button variant='ghost'>Мои магазины</Button>
-            </Link>
-          ) : (
-            <CreateStoreModal>
-              <Button variant='ghost'>
-                Создать магазин
-              </Button>
-            </CreateStoreModal>
-          )}
-          <ToggleTheme />
-          <Link href={DASHBOARD_URL.home()}>
-          <div className='min-w-[40px]'>
-						  <Image
-              src={user.picture}
-              alt={user.name}
-              width={40}
-              height={40}
-              className='rounded-full'
-            />
+	return (
+		<div className='hidden items-center gap-x-4 ml-auto lg:flex'>
+			<HeaderCart />
+			<Link href={PUBLIC_URL.explorer()}>
+				<Button variant='ghost'>Каталог</Button>
+			</Link>
+			{isLoading ? (
+				<>
+					<div className='flex gap-x-7 items-center'>
+						<Skeleton className='h-8 w-84' />
+						<Loader size='default' />
 					</div>
-          </Link>
-        </>
-      ) : (
-        <Link href={PUBLIC_URL.auth()}>
-          <Button variant='default'>
-            <LogOut className='size-4 mr-2' />
-            Войти
-          </Button>
-        </Link>
-      )}
-    </div>
-  )
+				</>
+			) : user ? (
+				<>
+					<Link href={DASHBOARD_URL.favorites()}>
+						<Button variant='ghost'>Избранное</Button>
+					</Link>
+					{user.stores.length ? (
+						<Link href={STORE_URL.home(user.stores[0].id)}>
+							<Button variant='ghost'>Мои магазины</Button>
+						</Link>
+					) : (
+						<CreateStoreModal>
+							<Button variant='ghost'>Создать магазин</Button>
+						</CreateStoreModal>
+					)}
+					<ToggleTheme />
+					<Link href={DASHBOARD_URL.home()}>
+						<div className='min-w-[40px]'>
+							<Image
+								src={user.picture}
+								alt={user.name}
+								width={40}
+								height={40}
+								className='rounded-full'
+							/>
+						</div>
+					</Link>
+				</>
+			) : (
+				<Link href={PUBLIC_URL.auth()}>
+					<Button variant='default'>
+						<LogOut className='size-4 mr-2' />
+						Войти
+					</Button>
+				</Link>
+			)}
+		</div>
+	)
 }
--- client-side/src/components/layouts/main-layout/header/header-menu/header-cart/cart-item/CartItem.tsx
+++ client-side/src/components/layouts/main-layout/header/header-menu/header-cart/cart-item/CartItem.tsx
@@ -14,17 +14,22 @@ export function CartItem({ item }: CartItemProps) {
 		<div className='flex items-center mb-5'>
 			<Link
 				href={PUBLIC_URL.product(item.product.id)}
-				className='relative h-28 w-28 rounded-md overflow-hidden'
+				className='relative w-24 h-24 rounded-sm overflow-hidden flex-shrink-0'
 			>
 				<Image
 					src={item.product.images[0]}
 					alt={item.product.title}
 					fill
+					style={{ objectFit: 'contain' }}
 				/>
 			</Link>
-			<div className='ml-6'>
-				<h2>{item.product.title}</h2>
-				<p>{formatPrice(item.product.price)}</p>
+			<div className='ml-4 flex-grow'>
+				<h2 className='text-base font-medium leading-tight mb-1 break-words'>
+					{item.product.title}
+				</h2>
+				<p className='text-sm text-foreground/70 mb-2'>
+					{formatPrice(item.product.price)}
+				</p>
 				<CartActions item={item} />
 			</div>
 		</div>
